2b5fba620341e34a4d37e732f09460d23e8efa73
Maria Elvin <maria@tedgustaf.com>
2016-09-07 15:15:28 +0200

    9 skriv om med rabatt och extrauppgift 1

--- robots.py
+++ robots.py
@@ -5,13 +5,21 @@ class Product:
 		self.tax = tax
 
 	def price_with_tax(self):
-		return self.price * self.count *self.tax
+		total = self.price * self.count *self.tax
+		if total > 500:
+			return 0.9 * total
+		else:
+			return total
 
 products = [
 	Product(price=900, count=2, tax=1.25), 
-	Product(price=100, count=1, tax=1.06)]
+	Product(price=100, count=1, tax=1.06),
+	Product(price=100, count=1, tax=1.25)
+	]
 
-total_price = products[0].price_with_tax() + products[1].price_with_tax()
+total_price = 0
+for p in products:
+	total_price = total_price + p.price_with_tax()
 
 print(
 	total_price
